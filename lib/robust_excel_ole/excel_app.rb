a681d605a6fa061b8a3a0b8820bc7442b01ebf2d
Thomas R <thomas.raths@gmx.net>
2014-08-25 10:20:54 +0200

    added excel_app.rb

---
+++ lib/robust_excel_ole/excel_app.rb
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+module RobustExcelOle
+
+  class ExcelApp
+
+    def self.create
+      new(:reuse => false)
+    end
+
+    def self.reuse_if_possible
+      new(:reuse => true)
+    end
+
+    
+    def self.close_all
+      while running_app do
+        close_one_app
+        GC.start
+      end
+    end
+
+    def self.close_one_app
+      excel = running_app
+      if excel then 
+        excel.Workbooks.Close
+        excel_hwnd = excel.HWnd
+        excel.Quit
+        weak_excel_ref = WeakRef.new(excel)
+        excel = nil
+        GC.start
+        if weak_excel_ref.weakref_alive? then
+          #if WIN32OLE.ole_reference_count(weak_xlapp) > 0
+          begin
+            weak_xlapp.ole_free
+          rescue
+            #puts "could not do ole_free on #{weak_excel_ref}"
+          end
+        end
+      end
+      process_id = Win32API.new("user32", "GetWindowThreadProcessId", ["I","P"], "I")
+      pid_puffer = " " * 32
+      process_id.call(excel_hwnd, pid_puffer)
+      pid = pid_puffer.unpack("L")[0]
+      Process.kill("KILL", pid)
+      anz_objekte = 0
+      ObjectSpace.each_object(WIN32OLE) do |o|
+        anz_objekte += 1
+        #p [:ole_object_name, o, (o.Name rescue nil)]
+        #trc_info :ole_type, o.ole_obj_help rescue nil
+        #trc_info :obj_hwnd, o.HWnd rescue   nil
+        #trc_info :obj_Parent, o.Parent rescue nil
+        begin
+          o.ole_free
+        rescue
+          #puts "olefree_error: #{$!}"
+        end
+      end
+    end
+    
+
+    # returns nil, if no excel is running or connected to a dead Excel app
+    def self.running_app
+      result = WIN32OLE.connect('Excel.Application') rescue nil 
+      if result 
+        begin
+          result.Visible    # send any method, just to see if it responds
+        rescue 
+          puts "Window-handle = #{result.HWnd}"
+          # dead!!!
+          return nil
+        end
+      end
+      result
+    end
+
+    def initialize(options= {})
+      options = {:reuse => true}.merge(options)
+      if options[:reuse] then
+        @winapp = self.class.running_app
+        if @winapp
+          #p "bestehende Applikation wird wieder benutzt"
+          @winapp.DisplayAlerts = options[:displayalerts] unless options[:displayalerts]==nil
+          @winapp.Visible = options[:visible] unless options[:visible]==nil
+          WIN32OLE.const_load(@winapp, RobustExcelOle) unless RobustExcelOle.const_defined?(:CONSTANTS)
+          return
+        end
+      end
+
+      options = {
+        :displayalerts => false,
+        :visible => false,
+      }.merge(options)
+      #p "kreiere neue application"
+      @winapp = WIN32OLE.new('Excel.application')
+      @winapp.DisplayAlerts = options[:displayalerts]
+      @winapp.Visible = options[:visible]
+      WIN32OLE.const_load(@winapp, RobustExcelOle) unless RobustExcelOle.const_defined?(:CONSTANTS)
+    end
+
+    def == other_app      
+      self.HWnd == other_app.HWnd    if other_app.is_a?(ExcelApp)
+    end
+
+    def alive?
+      @winapp.Name
+      true
+    rescue 
+      puts $!.message
+      false
+    end
+    
+    def method_missing(name, *args)
+      @winapp.send(name, *args)
+    end
+
+  end
+end
---
+++ spec/excel_app_spec.rb
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+require File.join(File.dirname(__FILE__), './spec_helper')
+
+$VERBOSE = nil
+
+module RobustExcelOle
+
+  describe ExcelApp do
+    before do
+      @dir = create_tmpdir
+      @simple_file = @dir + '/simple.xls'
+    end
+
+    after do
+      rm_tmp(@dir)
+    end
+
+    save_path = "C:" + "/" + "simple_save.xls"
+
+    context "app creation" do
+      after do
+        ExcelApp.close_all
+      end
+
+      def creation_ok?
+        @app.alive?.should == true
+        @app.Visible.should == false
+        @app.Name.should == "Microsoft Excel"
+      end
+
+      it "should work with 'new' " do
+        @app = ExcelApp.new
+        creation_ok?
+      end
+
+      it "should work with 'new' " do
+        @app = ExcelApp.new(:reuse => false)
+        creation_ok?
+      end
+
+      it "should work with 'create' " do
+        @app = ExcelApp.create
+        creation_ok?
+      end
+
+    end
+
+
+
+    context "with existing app" do
+
+      before do
+        ExcelApp.close_all
+        @app1 = ExcelApp.create
+      end
+
+      after do
+        ExcelApp.close_all
+      end
+
+      it "should create different app" do
+        app2 = ExcelApp.create
+        #puts "@app1 #{@app1.Hwnd}"
+        #puts "app2  #{app2.Hwnd}"
+        app2.Hwnd.should_not == @app1.Hwnd
+      end
+
+      it "should reuse existing app" do
+        app2 = ExcelApp.reuse_if_possible
+        #puts "@app1 #{@app1.Hwnd}"
+        #puts "app2  #{app2.Hwnd}"
+        app2.Hwnd.should == @app1.Hwnd
+      end
+
+      it "should reuse existing app with default options for 'new'" do
+        app2 = ExcelApp.new
+        #puts "@app1 #{@app1.Hwnd}"
+        #puts "app2  #{app2.Hwnd}"
+        app2.Hwnd.should == @app1.Hwnd
+      end
+
+    end
+
+    context "close excel instances" do
+      it "simple file with default" do
+        RobustExcelOle::ExcelApp.close_all
+        expect { WIN32OLE.connect("Excel.Application") }.to raise_error
+        sleep 0.1      
+        exl1 = WIN32OLE.new("Excel.Application")
+        exl1.Workbooks.Add 
+        exl2 = WIN32OLE.new("Excel.Application")
+        exl2.Workbooks.Add 
+        expect { WIN32OLE.connect("Excel.Application") }.to_not raise_error
+        RobustExcelOle::ExcelApp.close_all
+        sleep 0.1
+        expect { WIN32OLE.connect("Excel.Application") }.to raise_error
+      end
+    end
+
+    describe "==" do
+      before do
+        ExcelApp.close_all
+        @app1 = ExcelApp.create
+      end
+
+      after do
+        ExcelApp.close_all
+      end
+
+      it "should be true with two identical excel applications" do
+        app2 = ExcelApp.reuse_if_possible
+        app2.should == @app1
+      end
+
+      it "should be false with two different excel applications" do
+        app2 = ExcelApp.create
+        app2.should_not == @app1
+      end
+
+      it "should be false with non-ExcelApp objects" do
+        @app1.should_not == "hallo"
+        @app1.should_not == 7
+        @app1.should_not == nil
+      end
+
+    end
+
+  end
+end

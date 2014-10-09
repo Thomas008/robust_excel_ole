7f252af3c82da6316ce068ade1e900b46816e48e
Thomas R <thomas.raths@gmx.net>
2014-10-09 20:34:15 +0200

    Readme.rdoc

--- examples/save_sheets/example_save_sheets.rb
+++ examples/save_sheets/example_save_sheets.rb
@@ -11,18 +11,21 @@ begin
   dir = 'C:/'
   file_name = dir + 'book_with_blank.xls'
   book = Book.open(file_name)                   # open a book
-  Excel.current.Visible = true     # make Excel visible 
-  # 1. Bücher erstmal speichern, um sie öffnen zu können
+  Excel.current.Visible = true                  # make Excel visible 
   i = 0
   book.each do |sheet|
     i = i + 1
     puts "#{i}. sheet:"
     file_name_sheet = file_name + "_sheet#{i}.xls"
     puts "file_name_sheet: #{file_name_sheet}"
-    book.save_as(file_name_sheet) 
+    empty_book = Excel.current.Workbooks.Add
+    puts "class: #{empty_book}.class"
+    sleep 2
+    # empty_book.Workbooks(1).save_as(file_name_sheet) 
   end
-  # generate empty book
-  # book_sheet = Excel.Workbooks.Add
+  # 1. generate empty book
+  #    book_sheet = Excel.Workbooks.Add
+  # 2. copy with VBA method Copy (see Excel object model) a sheet into the empty book  
   
  ensure                                                              
   Excel.close_all                              # close workbooks, quit Excel application

16f8878055b0fc4d94de79dff8c1a36dd1d31a30
Thomas R <thomas.raths@gmx.net>
2014-10-10 18:22:42 +0200

    example save_sheets

--- examples/save_sheets/example_save_sheets.rb
+++ examples/save_sheets/example_save_sheets.rb
@@ -1,4 +1,5 @@
-# example 11: save the sheets of a book as separate books
+# example_save_sheets.rb: 
+# save the sheets of a book as separate books
 
 require File.join(File.dirname(__FILE__), '../../lib/robust_excel_ole')
 
@@ -9,23 +10,29 @@ include RobustExcelOle
 Excel.close_all
 begin
   dir = 'C:/'
-  file_name = dir + 'book_with_blank.xls'
-  book = Book.open(file_name)                   # open a book
-  Excel.current.Visible = true                  # make Excel visible 
+  suffix = '.xls'
+  book_name = dir + 'book_with_blank' 
+  book = Book.open(book_name + suffix)                   # open a book
+  Excel.current.Visible = true                           # make Excel visible 
   i = 0
   book.each do |sheet|
     i = i + 1
     puts "#{i}. sheet:"
-    file_name_sheet = file_name + "_sheet#{i}.xls"
-    puts "file_name_sheet: #{file_name_sheet}"
-    empty_book = Excel.current.Workbooks.Add
-    puts "class: #{empty_book}.class"
-    sleep 2
-    # empty_book.Workbooks(1).save_as(file_name_sheet) 
+    sheet_name = book_name + "_sheet#{i}"
+    puts "sheet_name: #{sheet_name}"
+    puts "absolute_path(sheet_name): #{absolute_path(sheet_name)}"
+    # generate an empty workbook and save it
+    excel = Excel.create
+    excel.Workbooks.Add
+    empty_workbook = excel.Workbooks.Item(1)
+    empty_workbook.SaveAs(absolute_path(sheet_name), XlExcel8) 
+    empty_workbook.Close
+    # open the book, add the sheet and save it
+    sheet_book = Book.open(absolute_path(sheet_name) + suffix)
+    sheet_book.add_sheet sheet
+    sheet_book.save
+    sheet_book.close
   end
-  # 1. generate empty book
-  #    book_sheet = Excel.Workbooks.Add
-  # 2. copy with VBA method Copy (see Excel object model) a sheet into the empty book  
   
  ensure                                                              
   Excel.close_all                              # close workbooks, quit Excel application

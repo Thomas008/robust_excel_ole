6322835201fd76c584b58c4e7ac404636372b095
Thomas R <thomas.raths@gmx.net>
2014-11-12 14:47:13 +0100

    bug fix examle_excel

--- examples/open_save_close/example_control_to_excel.rb
+++ examples/open_save_close/example_control_to_excel.rb
@@ -18,20 +18,20 @@ begin
   sheet[0,0] = sheet[0,0].value == "simple" ? "complex" : "simple"       # change a cell
   sleep 1
   begin
-    new_book = Book.open(file_name, :if_unsaved => :excel) # open another book with the same file name 
+    new_book = Book.open(file_name, :if_unsaved => :alert) # open another book with the same file name 
   rescue ExcelUserCanceled => msg                          # if the user chooses not open the book,
   	puts "#{msg.message}"                                  #   an exeptions is raised
   end
   puts "new book has opened" if new_book
   Excel.current.Visible = true
   begin
-  	book.close(:if_unsaved => :excel)                      # close the unsaved book. 
+  	book.close(:if_unsaved => :alert)                      # close the unsaved book. 
   rescue ExcelUserCanceled => msg                          # user is asked whether the unsaved book shall be saved
   	puts "#{msg.message}"                                  # if the user chooses to cancel, then an expeption is raised
   end
   if new_book then
   	begin
-  	  new_book.save_as(file_name, :if_exists => :excel)    # save the new book, if it was opened
+  	  new_book.save_as(file_name, :if_exists => :alert)    # save the new book, if it was opened
   	rescue ExcelErrorSave => msg                           # user is asked, whether the existing file shall be overwritten
   	  puts "save_as: #{msg.message}"                       # if the user chooses "no" or "cancel", an exception is raised
   	end 

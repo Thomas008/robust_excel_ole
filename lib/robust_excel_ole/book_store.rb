f7cf87b6458ceef8a0cfafa26b51a68f18d3fe4d
Thomas R <thomas.raths@gmx.net>
2015-03-05 16:46:07 +0100

    bookstore: fetch,  store

--- lib/robust_excel_ole/book_store.rb
+++ lib/robust_excel_ole/book_store.rb
@@ -11,11 +11,66 @@ module RobustExcelOle
       @@filename2book = {}
     end
 
-    def fetch(file, options = { })
+    # returns a book with the given filename, if it was open once
+    # preference order: writable book, readonly unsaved book, readonly book (the last one), closed book
+    # options: 
+    #   :readonly  declares whether a writable book is prefered, or a book for read_only is sufficient
+    #             false (default)  -> prefer a writable book,  true -> read_only book is sufficient 
+    #     
+    def fetch(filename, options = { })
+      p "fetch:"
+      filename_key = RobustExcelOle::canonize(filename)
+      p "filename_key: #{filename_key}"
+      readonly_book = readonly_unsaved_book = closed_book = result = nil
+      books = @@filename2book[filename_key]
+      p "books: #{books}"
+      return nil  unless books
+      books.each do |book|
+        p "book: #{book}"
+        if book.alive?
+          p "book alive"
+          if (not book.ReadOnly)
+            p "book writable"
+            return book
+          else
+            p "book read_only"
+            book.Saved ? readonly_book = book : readonly_unsaved_book = book
+          end
+        else
+          p "book closed"
+          closed_book = book
+        end
+      end
+      result = readonly_unsaved_book ? readonly_unsaved_book : (readonly_book ? readonly_book : closed_book)
+      p "book: #{result}"
+      result
     end
 
+    # stores a book
     def store(book)
+      filename_key = RobustExcelOle::canonize(book.filename)
+      if @@filename2book[filename_key]
+        @@filename2book[filename_key] << book unless @@filename2book[filename_key].include?(book)
+      else
+        @@filename2book[filename_key] = [self]
+      end
     end
+
+    # prints the book store
+    def print
+      p "bookstore:"
+      @@filename2book.each do |filename,books|
+        p " filename: #{filename}"
+        p " books:"
+        books.each do |book|
+          p "#{book}"
+        end
+      end
+    end
+
+
+    #private :print
+
 
   end
  

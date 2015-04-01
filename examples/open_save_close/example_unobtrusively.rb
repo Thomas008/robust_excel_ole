2535a8691ec49f9ab366e6ba4fe27dd9239a6a20
Thomas R <thomas.raths@gmx.net>
2015-04-02 00:31:59 +0200

    unobtrusively: example and documentation

--- examples/open_save_close/example_unobtrusively.rb
+++ examples/open_save_close/example_unobtrusively.rb
@@ -9,24 +9,20 @@ include RobustExcelOle
 Excel.close_all
 begin
   dir = create_tmpdir
-  file_name = dir + 'simple.xls'
-  other_file_name = dir + 'more_data/simple.xls'
-  book = Book.open(file_name, :visible => true)  # open a book, make Excel visible
-  sleep 1
-  sheet = book[0]
-  first_cell = sheet[0,0].value                                   # access a sheet
-  sheet[0,0] = first_cell == "simple" ? "complex" : "simple"      # change a cell
-  sleep 1
-  new_book = Book.open(other_file_name, :if_obstructed => :save)  # open a book with the same file name in a different path
-  sleep 1                                                         #save the old book, close it, before
-  old_book = Book.open(file_name, :if_obstructed => :forget ,:visible => true) # open the old book    
-  sleep 1
-  old_sheet = old_book[0]
-  old_first_cell = old_sheet[0,0].value
-  puts "the old book was saved" unless old_first_cell == first_cell 
-  new_book.close                                 # close the books                      
-  old_book.close
+  simple_file = dir + 'simple.xls'
+  book = Book.open(simple_file, :visible => true)  # open a book, make Excel visible
+  old_sheet = book[0]
+  p "1st cell: #{old_sheet[0,0].value}"
+  sleep 2
+  Book.unobtrusively(simple_file) do |book|   # modify the book and keep its status unchanged
+    sheet = book[0]
+    sheet[0,0] = sheet[0,0].value == "simple" ? "complex" : "simple"
+  end
+  new_sheet = book[0]
+  p "1st cell: #{new_sheet[0,0].value}"
+  p "book saved" if book.Saved
+  book.close                                 # close the book                      
 ensure
-  Excel.close_all                         # close all workbooks, quit Excel application
+  Excel.close_all                            # close all workbooks, quit Excel application
   rm_tmp(dir)
 end

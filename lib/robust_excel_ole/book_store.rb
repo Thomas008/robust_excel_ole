c3178bf239f0206aa2e9fde62afaf183b145b9ec
Thomas R <thomas.raths@gmx.net>
2015-04-28 19:07:29 +0200

    fetch: bug fix

--- lib/robust_excel_ole/book_store.rb
+++ lib/robust_excel_ole/book_store.rb
@@ -16,43 +16,68 @@ module RobustExcelOle
     #                                 even if it is not writable, if such a book exists
     #                          prefer the writable book as described above, otherwise 
     def fetch(filename, options = { })
+      p "fetch:"
+      print
       filename_key = RobustExcelOle::canonize(filename)
+      p "filename_key: #{filename_key}"
       weakref_books = @filename2books[filename_key]
+      p "weakref_books: #{weakref_books}" rescue nil
       return nil unless weakref_books
-      readonly_book = readonly_unsaved_book = closed_book = result = nil      
-      weakref_books.each do |book|
-        if (not book.weakref_alive?) 
-          @filename2books[filename_key].delete(book)
+      result = readonly_book = readonly_unsaved_book = closed_book = nil      
+      weakref_books.each do |wr_book|
+        p "wr_book: #{wr_book}" rescue nil
+        if (not wr_book.weakref_alive?)
+          p "weakref not alive" 
+          @filename2books[filename_key].delete(wr_book)
         else
-          return book if options[:readonly_excel] && book.excel == options[:readonly_excel]
-          if book.alive?
-            if (not book.readonly)
-              if options[:readonly_excel]
-                result = book
-              else
-                return book
-              end
+          p "weakref alive"
+          if options[:readonly_excel] && wr_book.excel == options[:readonly_excel]
+            p "wr_book.excel == options :read_only"
+            result = wr_book
+            p "result: #{result}"
+            break 
+          end
+          if wr_book.alive?
+            p "wr_book alive"
+            if (not wr_book.readonly)
+              p "writable"
+              result = wr_book
+              p "result: #{result}"
+              break unless options[:readonly_excel]
             else
-              book.saved ? readonly_book = book : readonly_unsaved_book = book
+              p "wr_book readonly"
+              p "wr_book: saved: #{wr_book.saved}"
+              wr_book.saved ? readonly_book = wr_book : readonly_unsaved_book = wr_book
+              p "readonly_book: #{readonly_book}"
+              p "readonly_unsaved_book: #{readonly_unsaved_book}"
             end
           else
-            closed_book = book
+            p "wr_book not alive"
+            closed_book = wr_book
           end
         end
       end
-      result ? result : (readonly_unsaved_book ? readonly_unsaved_book : (readonly_book ? readonly_book : closed_book))
+      result = result ? result : (readonly_unsaved_book ? readonly_unsaved_book : (readonly_book ? readonly_book : closed_book))
+      p "result: #{result}"      
+      p "end:"
+      print
+      result.__getobj__ if result
     end
 
     # stores a book
     def store(book)
+      p "store:"
+      print
       filename_key = RobustExcelOle::canonize(book.filename)      
       if book.stored_filename
         old_filename_key = RobustExcelOle::canonize(book.stored_filename)
-        # deletes the weak reference
+        # deletes the weak reference to the book
         @filename2books[old_filename_key].delete(book)
       end
       @filename2books[filename_key] |= [WeakRef.new(book)]
       book.stored_filename = book.filename
+      p "end:"
+      print
     end
 
     # prints the book store
@@ -64,9 +89,13 @@ module RobustExcelOle
           p " books:"
           p " []" if books == []
           books.each do |book|
-            p "#{book}"
-            p "excel: #{book.excel}"
-            p "alive: #{book.alive?}"
+            if book.weakref_alive?
+              p "#{book}"
+              p "excel: #{book.excel}"
+              p "alive: #{book.alive?}"
+            else
+              p "weakref not alive"
+            end
           end
         end
       end

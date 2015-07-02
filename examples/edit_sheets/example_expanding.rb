8c97de6a18b94c6fd08b393922ce529cc5436d75
Thomas R <thomas.raths@gmx.net>
2015-07-03 00:58:21 +0200

    example_expanding: case for repeated names

--- examples/edit_sheets/example_expanding.rb
+++ examples/edit_sheets/example_expanding.rb
@@ -14,58 +14,44 @@ include RobustExcelOle
 begin
   Excel.close_all
   dir = "C:/data"
-  workbook_name = 'workbook_named_filled_concat.xls'
-  base_name, suffix = workbook_name.split(".")
+  workbook_name = 'workbook_named_concat.xls'
+  base_name = workbook_name[0,workbook_name.rindex('.')]
+  suffix = workbook_name[workbook_name.rindex('.')+1,workbook_name.length]
   file_name = dir + "/" + workbook_name
   extended_file_name = dir + "/" + base_name + "_expanded" + "." + suffix
   book_orig = Book.open(file_name)
   book_orig.save_as(extended_file_name, :if_exists => :overwrite) 
   book_orig.close
-  sheets = []
-  Excel.close_all
-  excel = Excel.new(:reuse => false, :visible => true)
+  sheet_names = []
   Book.unobtrusively(extended_file_name) do |book|     
-    p "book: #{book.stored_filename}"
     book.each do |sheet|
-      sheets << sheet
+      sheet_names << sheet.name
     end
     book.Names.each do |excel_name|
       full_name = excel_name.Name
       sheet_name, short_name = full_name.split("!")
-      p "full_name: #{full_name}"
-      p "short_name: #{short_name}"
       sheet = excel_name.RefersToRange.Worksheet
-      p "sheet: #{sheet.name}"
       sheet_name = short_name ? short_name : full_name
-      sheet_new = book.add_sheet(sheet, :as => (short_name ? sheet_name : full_name))
+      begin
+        sheet_new = book.add_sheet(sheet, :as => sheet_name)
+      rescue ExcelErrorSheet => msg
+        if msg.message == "sheet name already exists" 
+          sheet_new = book.add_sheet(sheet, :as => (sheet_name+sheet.name))
+        else 
+          puts msg.message
+        end
+      end
       sheet_new.Names.Add("Name" => "name", "RefersTo" => "=" + "$B$2")
       sheet_new[1,1].Value = sheet_name
-      sheet_new.name = sheet_name
+      begin
+        sheet_new.name = sheet_name
+      rescue
+        sheet_new.name = (sheet_name+sheet.name)
+      end
     end
-    sheets.each do |sheet|
-      sheet.Delete()
+    sheet_names.each do |sheet_name|
+      book[sheet_name].Delete()
     end
   end
   Excel.close_all
 end
-
-    #book.each do |sheet|
-    #  p "sheet.name: #{sheet.name}"
-    #  #sheet_names << sheet.name 
-    #  names = sheet.Names
-    #  p "names: #{names} #{names.Count}"
-      #sheet.Names.each do |excel_name|
-      #  full_name = excel_name.Name
-      #  p "full_name: #{full_name}"
-      #  sheet_name, short_name = full_name.split("!")
-      #  p "short_name: #{short_name}"
-      #  sheet_new = book.add_sheet(sheet, :as => short_name)
-      #  sheet_new.Names.Add("Name" => "name", "RefersTo" => "=" + "$B$2")
-      #  sheet_new[1,1].Value = short_name
-      #  #sheet_new["name"] = short_name
-      #  book_new["name"] = short_name
-      #end
-    #end
-  #  sheet_names.each do |sheet_name|
-  #    book[sheet_name].Delete()
-  #  end

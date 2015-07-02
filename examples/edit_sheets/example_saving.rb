45f4dc937210af356a5bc654852fd7b86d2b4062
Thomas R <thomas.raths@gmx.net>
2015-07-02 11:16:31 +0200

    example_saving

--- examples/edit_sheets/example_saving.rb
+++ examples/edit_sheets/example_saving.rb
@@ -14,18 +14,18 @@ begin
   base_name, suffix = workbook_name.split(".")
   file_name = dir + "/" + workbook_name
   excel = Excel.new(:visible => true) 
-  #Book.unobtrusively(file_name, :if_closed => excel) do |book_orig| 
-  #  book_orig.each do |sheet_orig|
-  #    file_sheet_name = dir + "/" + base_name + "_" + sheet_orig.name + "." + suffix
-  #    p "sheet.Name: #{sheet_orig.Name}"
-  #    Excel.current.generate_workbook(file_sheet_name)
-  #  end
-  #end
+  Book.unobtrusively(file_name, :if_closed => excel) do |book_orig| 
+    book_orig.each do |sheet_orig|
+      file_sheet_name = dir + "/" + base_name + "_" + sheet_orig.name + "." + suffix
+      p "sheet.Name: #{sheet_orig.Name}"
+      Excel.current.generate_workbook(file_sheet_name)
+    end
+  end
   Book.unobtrusively(file_name, :if_closed => excel) do |book_orig| 
     book_orig.each do |sheet_orig|
       p "sheet.Name: #{sheet_orig.Name}"
       file_sheet_name = dir + "/" + base_name + "_" + sheet_orig.name + "." + suffix
-      Excel.current.generate_workbook(file_sheet_name)
+      #Excel.current.generate_workbook(file_sheet_name)
       # delete all existing sheets, and add the sheet    
       book = Book.open(file_sheet_name, :visible => true)
       book.add_sheet sheet_orig

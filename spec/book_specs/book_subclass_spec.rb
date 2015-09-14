a82e777b08cf242d292659d23a5fcbe683adb5a5
Thomas R <thomas.raths@gmx.net>
2015-09-14 17:59:22 +0200

    new book_spec structure

--- spec/book_specs/book_subclass_spec.rb
+++ spec/book_specs/book_subclass_spec.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-require File.join(File.dirname(__FILE__), './spec_helper')
+require File.join(File.dirname(__FILE__), './../spec_helper')
 
 
 module My # :nodoc: #
---
+++ spec/robust_excel_ole_spec.rb
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+require File.join(File.dirname(__FILE__), './spec_helper')
+
+$VERBOSE = nil
+
+include RobustExcelOle
+
+describe Book do
+
+  before(:all) do
+    excel = Excel.new(:reuse => true)
+    open_books = excel == nil ? 0 : excel.Workbooks.Count
+    puts "*** open books *** : #{open_books}" if open_books > 0
+    Excel.close_all
+  end
+
+  before do
+    @dir = create_tmpdir
+    @simple_file = @dir + '/workbook.xls'
+  end
+
+  after do
+    #Excel.close_all
+    rm_tmp(@dir)
+  end
+
+  describe "#absolute_path" do
+    
+    context "with standard" do
+      
+      before do
+        @previous_dir = Dir.getwd
+      end
+      
+      after do
+        Dir.chdir @previous_dir
+      end
+
+      it "should return the right absolute paths" do
+        absolute_path("C:/abc").should == "C:\\abc"
+        absolute_path("C:\\abc").should == "C:\\abc"
+        Dir.chdir "C:/windows"
+        absolute_path("C:abc").downcase.should == Dir.pwd.gsub("/","\\").downcase + "\\abc"
+        absolute_path("C:abc").upcase.should   == File.expand_path("abc").gsub("/","\\").upcase
+      end
+
+      it "should return right absolute path name" do
+        filename = 'C:/Dokumente und Einstellungen/Zauberthomas/Eigene Dateien/robust_excel_ole/spec/book_spec.rb'
+        absolute_path(filename).gsub("\\","/").should == filename
+      end
+    end
+  end
+
+  describe "canonize" do
+
+    context "with standard" do
+      
+      it "should reduce slash at the end" do
+        normalize("hallo/").should == "hallo"
+        normalize("/this/is/the/Path/").should == "/this/is/the/Path"
+      end
+
+      it "should save capital letters" do
+        normalize("HALLO/").should == "HALLO"
+        normalize("/This/IS/tHe/patH/").should == "/This/IS/tHe/patH"
+      end
+
+      it "should reduce multiple shlashes" do
+        normalize("/this/is//the/path").should == "/this/is/the/path"
+        normalize("///this/////////is//the/path/////").should == "/this/is/the/path"
+      end
+
+      it "should reduce dots in the paths" do
+        canonize("/this/is/./the/path").should == "/this/is/the/path"
+        canonize("this/.is/./the/pa.th/").should == "this/.is/the/pa.th"
+        canonize("this//.///.//.is/the/pa.th/").should == "this/.is/the/pa.th"
+      end
+
+      it "should change to the upper directory with two dots" do
+        canonize("/this/is/../the/path").should == "/this/the/path"
+        canonize("this../.i.s/.../..the/..../pa.th/").should == "this../.i.s/.../..the/..../pa.th"
+      end
+
+      it "should downcase" do
+        canonize("/This/IS/tHe/path").should == "/this/is/the/path"
+        canonize("///THIS/.///./////iS//the/../PatH/////").should == "/this/is/path"
+      end
+    end
+  end
+
+end

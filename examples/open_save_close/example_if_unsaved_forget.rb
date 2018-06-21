bfc5e73499691caa4ba6cae10143638a7711dc1d
Thomas008 <Thomas.Raths@gmx.net>
2018-06-21 19:33:09 +0200

    adapted

--- examples/open_save_close/example_if_unsaved_forget.rb
+++ examples/open_save_close/example_if_unsaved_forget.rb
@@ -12,14 +12,18 @@ begin
   dir = create_tmpdir
   file_name = dir + 'workbook.xls' 
   book = Workbook.open(file_name)                      # open a book
-  book.excel.visible = true                        # make current Excel visible 
+  puts "file_name: #{file_name}"
+  #book.excel.visible = true                        # make current Excel visible 
   sleep 1
   sheet = book.sheet(1)                                            # access a sheet
   first_cell = sheet[1,1].value
   sheet[1,1] = first_cell == "simple" ? "complex" : "simple" # change a cell
   sleep 1
+  puts "new_book: before"
+  puts "file_name: #{file_name}"
   new_book = Workbook.open(file_name, :if_unsaved => :forget) # open another book with the same file name 
                                                           # and close the unsaved book without saving it
+  puts "new_book: after"
   sheet_new_book = new_book.sheet(1)
   if (not book.alive?) && new_book.alive? && sheet_new_book[1,1].value == first_cell then # check whether the unsaved book 
     puts "open with :if_unsaved => :forget : the unsaved book is closed and not saved."     # is closed and was not saved

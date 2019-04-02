26d9b8a55a0890ea76795abeeacd3d1937f69305
Thomas008 <Thomas.Raths@gmx.net>
2019-04-02 17:32:35 +0200

    no trace to stdout

--- lib/robust_excel_ole/reo_common.rb
+++ lib/robust_excel_ole/reo_common.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-LOG_TO_STDOUT = true      unless Object.const_defined?(:LOG_TO_STDOUT)
+LOG_TO_STDOUT = false            unless Object.const_defined?(:LOG_TO_STDOUT)
 REO_LOG_DIR   = ''.freeze        unless Object.const_defined?(:REO_LOG_DIR)
 REO_LOG_FILE  = 'reo.log'.freeze unless Object.const_defined?(:REO_LOG_FILE)
 

0752f1db191b33d33fd081c7d4310b0e295c400c
Thomas008 <Thomas.Raths@gmx.net>
2020-02-13 22:51:59 +0100

    reo-directory

--- lib/robust_excel_ole/reo_common.rb
+++ lib/robust_excel_ole/reo_common.rb
@@ -173,8 +173,7 @@ module RobustExcelOle
           home = homes.find { |h| !ENV[h].nil? }
           reo_log_dir = ENV[home]
         else
-          #reo_log_dir = REO_LOG_DIR
-          reo_log_dir = "C:/Users/User"
+          reo_log_dir = REO_LOG_DIR
         end
         File.open(reo_log_dir + '/' + REO_LOG_FILE,'a') do |file|
           file.puts text

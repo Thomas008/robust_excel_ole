81bf6a3a535625907e892e7aefc8431da746bd3e
Thomas008 <Thomas.Raths@gmx.net>
2020-07-12 12:59:27 +0200

    bin

---
+++ bin/reo.rb
@@ -0,0 +1,21 @@
+require 'pry'
+require '../lib/robust_excel_ole'
+
+include REO
+include General
+
+puts 'REO console started'
+puts 
+
+# some pry configuration
+Pry.config.windows_console_warning = false
+Pry.config.history.should_save = true
+Pry.config.color = false
+#Pry.editor = 'notepad'  # 'subl', 'vi'
+#Pry.config.prompt =
+#  [
+#    ->(_obj, _nest_level, _) { ">> " },
+#    ->(*) { "  " }
+#  ]
+
+pry

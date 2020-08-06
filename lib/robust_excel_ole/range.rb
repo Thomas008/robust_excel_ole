9663bede2e602560eff33fe7bc4b969f05d15e8a
Thomas008 <Thomas.Raths@gmx.net>
2020-08-06 21:51:31 +0200

    Range#copy using *args

--- lib/robust_excel_ole/range.rb
+++ lib/robust_excel_ole/range.rb
@@ -113,44 +113,29 @@ module RobustExcelOle
     # @params [Address or Address-Array] address or upper left position of the destination range
     # @options [Worksheet] the destination worksheet
     # @options [Hash] options: :transpose, :values_only
-    def copy(dest_address1, sheet_or_dest_address2 = :__not_provided, options_or_sheet = :__not_provided, not_provided_or_options = :__not_provided)
+    def copy(dest_address, *remaining_args)
+      dest_sheet = @worksheet
+      options = { }
+      remaining_args.each do |arg|
+        case arg
+        when Object::Range, Integer then dest_address = [dest_address,arg]
+        when Worksheet, WIN32OLE    then dest_sheet = arg.to_reo
+        when Hash                   then options = arg
+        else raise RangeNotCopied, "cannot copy range: argument error: #{remaining_args.inspect}"
+        end
+      end
       begin
-        dest_address = if sheet_or_dest_address2.is_a?(Object::Range) or sheet_or_dest_address2.is_a?(Integer)
-          [dest_address1,sheet_or_dest_address2] 
-        else
-          dest_address1
-        end
-        dest_sheet = if sheet_or_dest_address2.is_a?(Worksheet) or sheet_or_dest_address2.is_a?(WIN32OLE)
-          sheet_or_dest_address2.to_reo
-        else
-          if options_or_sheet.is_a?(Worksheet) or options_or_sheet.is_a?(WIN32OLE)
-            options_or_sheet.to_reo
-          else
-            @worksheet
-          end
-        end
-        options = if options_or_sheet.is_a?(Hash)
-          options_or_sheet 
-        else
-          if not_provided_or_options.is_a?(Hash)
-            not_provided_or_options
-          else
-            { }
-          end
-        end
         rows, columns = address_tool.as_integer_ranges(dest_address)
         dest_address_is_position = (rows.min == rows.max && columns.min == columns.max)
         dest_range_address = if (not dest_address_is_position) 
-            [rows.min..rows.max,columns.min..columns.max]
+          [rows.min..rows.max,columns.min..columns.max]
+        else
+          if (not options[:transpose])
+            [rows.min..rows.min+self.Rows.Count-1, columns.min..columns.min+self.Columns.Count-1]
           else
-            if (not options[:transpose])
-              [rows.min..rows.min+self.Rows.Count-1,
-               columns.min..columns.min+self.Columns.Count-1]
-            else
-              [rows.min..rows.min+self.Columns.Count-1,
-               columns.min..columns.min+self.Rows.Count-1]
-            end
+            [rows.min..rows.min+self.Columns.Count-1, columns.min..columns.min+self.Rows.Count-1]
           end
+        end
         dest_range = dest_sheet.range(dest_range_address)
         if options[:values_only]
           dest_range.v = options[:transpose] ? self.v.transpose : self.v
@@ -165,8 +150,8 @@ module RobustExcelOle
           else
             if options[:transpose]
               added_sheet = @worksheet.workbook.add_sheet
-              self.copy_special(dest_address, added_sheet, :transpose => true)
-              added_sheet.range(dest_range_address).copy_special(dest_address,dest_sheet)
+              self.copy(dest_address, added_sheet, :transpose => true)
+              added_sheet.range(dest_range_address).copy(dest_address,dest_sheet)
               @worksheet.workbook.excel.with_displayalerts(false) {added_sheet.Delete}
             else
               self.Copy
@@ -175,58 +160,6 @@ module RobustExcelOle
           end
         end
       rescue WIN32OLERuntimeError, Java::OrgRacobCom::ComFailException => msg
-        raise RangeNotCopied, 'cannot copy range'
-      end
-    end
-
-    # becomes copy
-    # copies a range
-    # @params [Address or Address-Array] address or upper left position of the destination range
-    # @options [Worksheet] the destination worksheet
-    # @options [Hash] options: :transpose, :values_only
-    def copy_special(dest_address, dest_sheet = :__not_provided, options = { })
-      rows, columns = address_tool.as_integer_ranges(dest_address)
-      dest_sheet = @worksheet if dest_sheet == :__not_provided
-      dest_address_is_position = (rows.min == rows.max && columns.min == columns.max)
-      dest_range_address = if (not dest_address_is_position) 
-          [rows.min..rows.max,columns.min..columns.max]
-        else
-          if (not options[:transpose])
-            [rows.min..rows.min+self.Rows.Count-1,
-             columns.min..columns.min+self.Columns.Count-1]
-          else
-            [rows.min..rows.min+self.Columns.Count-1,
-             columns.min..columns.min+self.Rows.Count-1]
-          end
-        end
-      dest_range = dest_sheet.range(dest_range_address)
-      begin
-        if options[:values_only]
-          dest_range.Value = options[:transpose] ? self.Value.transpose : self.Value
-        else
-          if dest_range.worksheet.workbook.excel == @worksheet.workbook.excel     
-            if options[:transpose]
-              self.Copy
-              #dest_range.PasteSpecial('transpose' => true) 
-              dest_range.PasteSpecial(XlPasteAll,XlPasteSpecialOperationNone,false,true)
-            else
-              #self.Copy('destination' => dest_range.ole_range)
-              self.Copy(dest_range.ole_range)
-            end            
-          else
-            if options[:transpose]
-              added_sheet = @worksheet.workbook.add_sheet
-              self.copy_special(dest_address, added_sheet, :transpose => true)
-              added_sheet.range(dest_range_address).copy_special(dest_address,dest_sheet)
-              @worksheet.workbook.excel.with_displayalerts(false) {added_sheet.Delete}
-            else
-              self.Copy
-              #dest_sheet.Paste('destination' => dest_range.ole_range)
-              dest_sheet.Paste(dest_range.ole_range)
-            end
-          end
-        end
-      rescue WIN32OLERuntimeError, Java::OrgRacobCom::ComFailException => msg 
         raise RangeNotCopied, 'cannot copy range'
       end
     end

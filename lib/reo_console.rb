bb7c4ba5cc6aee16a5953d7e285e5083c78ad9fb
Thomas008 <Thomas.Raths@gmx.net>
2020-09-04 18:34:35 +0200

    new prompt

--- lib/reo_console.rb
+++ lib/reo_console.rb
@@ -6,14 +6,11 @@ include General
 
 # some pry configuration
 Pry.config.windows_console_warning = false
-#Pry.config.history_save = true
 Pry.config.color = false
+Pry.config.prompt_name = "REO "
+
+#Pry.config.history_save = true
 #Pry.editor = 'notepad'  # 'subl', 'vi'
-#Pry.config.prompt =
-#[
-#->(_obj, _nest_level, _) { ">> " },
-#->(*) { "  " }
-#]
 
 hooks = Pry::Hooks.new
 

95fa78f487f5ee98b847003e01e82c5f2512cba7
Thomas008 <Thomas.Raths@gmx.net>
2021-03-22 21:47:36 +0100

    new threshold for applying strategies

--- lib/robust_excel_ole/list_object.rb
+++ lib/robust_excel_ole/list_object.rb
@@ -95,7 +95,7 @@ module RobustExcelOle
       return @row_class.new(key_hash_or_number) if key_hash_or_number.respond_to?(:succ)
       opts = {limit: :first}.merge(opts)   
       key_hash = key_hash_or_number
-      matching_listrows = if @ole_table.ListRows.Count < 150
+      matching_listrows = if @ole_table.ListRows.Count < 120
         listrows_via_traversing(key_hash, opts)
       else
         listrows_via_filter(key_hash, opts)

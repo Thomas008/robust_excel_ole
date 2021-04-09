4b0d2e54a0591fe4fcd858dc59060edf8f6a9b18
Thomas008 <Thomas.Raths@gmx.net>
2021-04-09 16:53:01 +0200

    Worksheet#last_row: removing error handling

--- lib/robust_excel_ole/worksheet.rb
+++ lib/robust_excel_ole/worksheet.rb
@@ -449,11 +449,7 @@ module RobustExcelOle
     end
 
     def last_row
-      special_last_row = begin 
-        @ole_worksheet.UsedRange.SpecialCells(RobustExcelOle::XlLastCell).Row
-      rescue 
-        nil
-      end
+      special_last_row = @ole_worksheet.UsedRange.SpecialCells(RobustExcelOle::XlLastCell).Row
       used_last_row = @ole_worksheet.UsedRange.Rows.Count
 
       special_last_row && special_last_row >= used_last_row ? special_last_row : used_last_row

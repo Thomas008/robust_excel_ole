02a3db225d0ce8ea3ebea95318b8d44429e68fed
unknown <Thomas.Raths@gmx.net>
2022-03-25 16:16:38 +0100

    cygpath tests adapted

--- spec/cygwin_spec.rb
+++ spec/cygwin_spec.rb
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+
 require_relative 'spec_helper'
+
+$VERBOSE = nil
+
+include RobustExcelOle
+include General
 
 describe "on cygwin",  :if => RUBY_PLATFORM =~ /cygwin/ do
   describe ".cygpath" do
@@ -18,25 +24,26 @@ describe "on cygwin",  :if => RUBY_PLATFORM =~ /cygwin/ do
     end
 
     context "windows path is 'C:\\Users'" do
-      context "with '-u option" do
-        it { RobustExcelOle::Cygwin.cygpath('-u', 'C:\\Users').should eq '/cygdrive/c/Users'}
+      context "with '-p option" do
+        it { RobustExcelOle::Cygwin.cygpath('-p', 'C:\\Users').should eq '/cygdrive/c/Users'}
       end
     end
 
     context "cygwin path is './'" do
-      context "with '-u' options" do
-        it { RobustExcelOle::Cygwin.cygpath('-u', './').should eq './' }
+      context "with '-p' options" do
+        it { RobustExcelOle::Cygwin.cygpath('-p', './').should eq './' }
       end
 
-      context "with '-ua' options" do
-        it { RobustExcelOle::Cygwin.cygpath('-ua', './').should eq File.expand_path('./') + '/' }
-      end
+     # context "with '-pa' options" do
+     #   it { RobustExcelOle::Cygwin.cygpath('-pa', './').should eq File.expand_path('./') + '/' }
+     # end
 
-      context "with '-us' options" do
-        it { RobustExcelOle::Cygwin.cygpath('-us', './').should eq './' }
-      end
+      #context "with '-ps' options" do
+      #  it { RobustExcelOle::Cygwin.cygpath('-ps', './').should eq './' }
+      #end
     end
 
   end
 
 end
+
